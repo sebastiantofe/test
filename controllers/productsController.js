d573ff5c3f84888de4f987f5e43d86866df09ce8
Sebastian Torrado <sebastiantofe@gmail.com>
2021-05-29 22:15:47 -0500

    Added show all products controller

--- controllers/productsController.js
+++ controllers/productsController.js
@@ -1,10 +1,20 @@
-const db = require('../utils/db');
+const Model = require('../models/model');
+const Product = new Model('products');
+const async = require('async');
+
 
 exports.create_product = function(req, res) {
 
 };
 
 
-exports.show_all_products = function(req, res) {
+exports.show_all_products = async function(req, res) {
+
+	let data = await Product.select('*')
+
+	res.json({
+		products: data.rows
+	});
+	return;	
 
 };
